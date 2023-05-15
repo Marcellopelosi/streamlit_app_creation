3446e6462b466c3679d7d693e72f1fb762032499
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-05-15 19:02:46 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+# Carica il modello da file pickle
+with open('modello.pkl', 'rb') as file:
+    modello = pickle.load(file)
+
+# Funzione per fare previsioni sul dataset caricato
+def fare_previsioni(dataset):
+
+    previsioni = modello.predict(dataset)
+    return previsioni
+
+# Configurazione dell'applicazione Streamlit
+st.title("App per previsioni con modello pickle")
+
+# Caricamento del dataset
+file = st.file_uploader("Carica il dataset", type=["csv"])
+
+if file is not None:
+    # Leggi il file CSV in un DataFrame pandas
+    dataset = pd.read_csv(file)
+
+    # Mostra il dataset
+    st.subheader("Dataset caricato")
+    st.write(dataset)
+
+    # Esegui previsioni sul dataset caricato
+    if st.button("Fai previsioni"):
+        previsioni = fare_previsioni(dataset)
+
+        # Mostra le previsioni
+        st.subheader("Previsioni")
+        st.write(previsioni)
