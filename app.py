d41062f99b5326e5f9fbe470b3b56c6affd0a2a8
lorenzadimauro <95594736+lorenzadimauro@users.noreply.github.com>
2023-05-16 13:10:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,28 @@ import pandas as pd
 # Carica il modello da file pickle
 with open('model.pkl', 'rb') as file:
     modello = pickle.load(file)
+    
+def preprocessing(dataset):
+    dataset = dataset.drop(columns=[26,27], axis=1)
+    columns_test = ['unit_ID','time_in_cycles','setting_1', 'setting_2','setting_3','T2','T24','T30','T50','P2','P15','P30','Nf',
+           'Nc','epr','Ps30','phi','NRf','NRc','BPR','farB','htBleed','Nf_dmd','PCNfR_dmd','W31','W32' ]
+    dataset.columns = columns_test
+    #normalizzazione
+    dataset['cycle_norm'] = dataset['time_in_cycles']
+    cols_normalize_2 = dataset.columns.difference(['unit_ID','time_in_cycles','RUL'])
+    norm_test_df = pd.DataFrame(min_max_scaler.transform(dataset[cols_normalize_2]), 
+                            columns=cols_normalize_2, 
+                            index=dataset.index)
+    test_join_df = dataset[dataset.columns.difference(cols_normalize_2)].join(norm_test_df)
+    dataset = test_join_df.reindex(columns = dataset.columns)
+    dataset = dataset.reset_index(drop=True)
+    
+    return dataset
 
 # Funzione per fare previsioni sul dataset caricato
 def fare_previsioni(dataset):
-    # Aggiungi qui la logica per il preprocessing del dataset
-    # ...
-
+    dataset = preprocessing(dataset)
+    
     previsioni = modello.predict(dataset)
     return previsioni
 
