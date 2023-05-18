a906bf2df3058bfa6da7f40f953c5e605a1013ed
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-05-18 16:51:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ if file is not None:
     # Visualizza grafici
     if st.button("Visualizza grafici"):
        d = dataset[[0,1]].groupby(0).max().sort_values(by=1, ascending=False)
-       st.bar_chart(d)
+       st.bar_chart(dataset = d, x = 0, y = 1)
 
           
     
