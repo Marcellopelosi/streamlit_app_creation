acda46feeb683499c75ea3e2f3ba36aae8adea79
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-05-19 20:24:18 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+import plotly.graph_objects as go
 
 # Create sample DataFrame
 data = {
     'Name': ['John', 'Jane', 'Alice', 'Bob'],
     'Age': [32, 28, 35, 41],
-    'Party': ['A', 'B', 'A', 'B']
+    'Income': [50000, 60000, 70000, 55000],
+    'Expenses': [25000, 20000, 30000, 35000]
 }
 df = pd.DataFrame(data)
 
@@ -19,15 +20,22 @@ def main():
     st.dataframe(df)
 
     # Select party
-    party_selection = st.selectbox("Select Party", df['Party'].unique())
+    party_selection = st.selectbox("Select Party", df['Name'])
     
     # Filter DataFrame based on party selection
-    filtered_df = df[df['Party'] == party_selection]
+    filtered_df = df[df['Name'] == party_selection]
     
-    # Plotting
-    st.subheader(f"Party {party_selection} Members")
-    fig = px.bar(filtered_df, x='Name', y='Age', color='Name')
-    st.plotly_chart(fig)
+    # Display filtered DataFrame
+    st.subheader(f"Filtered DataFrame (Party {party_selection})")
+    st.dataframe(filtered_df)
+
+    # Plotting line graph for each column
+    for column in filtered_df.columns:
+        if column != 'Name':
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=filtered_df['Name'], y=filtered_df[column], mode='lines', name=column))
+            fig.update_layout(title=f"{column} over Names", xaxis_title="Name", yaxis_title=column)
+            st.plotly_chart(fig)
 
 if __name__ == '__main__':
     main()
