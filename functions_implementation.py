c120d80164722e42fc81478be3c763412c2d1a09
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-05-22 15:42:49 +0200

    Update functions_implementation.py

--- functions_implementation.py
+++ functions_implementation.py
@@ -53,7 +53,7 @@ def preprocessing(dataset):
     
     return dataset
 
-def fare_previsioni(dataset):
+def fare_previsioni(dataset, columns_test = columns_test):
     dataset = preprocessing(dataset)
     
     #calcolo unit id corrispondenti alle serie troppo corte
